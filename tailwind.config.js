aed006e9650d5dd84dec60598e5cf7c60f32cb1d
amohyyyy <ahmedmohy10101@gmail.com>
2025-08-16 00:49:20 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#eef2ff",
+          100: "#e0e7ff",
+          500: "#4f46e5",
+          600: "#4338ca"
+        }
+      }
+    }
+  },
+  plugins: []
+};
